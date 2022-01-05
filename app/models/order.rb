e2a1938da6976880f319bdddcf89c6a3331bfbf4
Gregoire Vallette <gregoire.vallette@gmail.com>
2022-01-05 11:50:27 +0100

    seed imports data from CSV file

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,13 +3,10 @@
 # order model
 class Order < ApplicationRecord
   validates :date, presence: true
-<<<<<<< HEAD
-=======
   validates :customer_id, presence: true
->>>>>>> seeds
   validates :country, presence: true, length: { minimum: 2 }
   validates :product_code, presence: true
-  validates :product_description, presence: true, length: { minimum: 10 }
+  validates :product_description, presence: true, length: { minimum: 2 }
   validates :quantity, presence: true
   validates :unit_price, numericality: { greater_than_or_equal_to: 0 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string-literal:true
+require('CSV')
+
+puts '-' * 72
+puts 'Destroying older entries'
+puts '-' * 72
+
+Order.delete_all
+
+puts 'Entries destroyed'
+
+puts '-' * 72
+puts 'Creating Orders from CSV'
+puts '-' * 72
+
+# rubocop:disable Metrics/MethodLength
+def csv_load
+  orders_count = 0
+  CSV.foreach(File.join(__dir__, 'data.csv'), { headers: true }) do |row|
+    new_order = Order.new(build_hash(row))
+    if new_order.save!
+      puts "successfully saved order #{row['order_id']}"
+      orders_count += 1
+    else
+      puts "Something went wrong saving order #{row['order_id']}"
+    end
+  end
+  orders_count
+end
+# rubocop:enable Metrics/MethodLength
+
+def build_hash(row)
+  {
+    date: DateTime.parse(row['date']),
+    customer_id: row['customer_id'].to_i,
+    country: row['country'],
+    product_code: row['product_code'],
+    product_description: row['product_description'],
+    quantity: row['quantity'].to_i,
+    unit_price: row['unit_price'].to_f
+  }
+end
+
+puts '-' * 72
+puts "Finished, saved #{csv_load} orders"
+puts '-' * 72
